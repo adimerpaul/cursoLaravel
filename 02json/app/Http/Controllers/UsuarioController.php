c39b315914412d6af4b34a6c27213e4645d5df6b
adimerpaul <adimer101@gmail.com>
2025-08-20 20:52:26 -0400

    Creado primera validacion

--- 02json/app/Http/Controllers/UsuarioController.php
+++ 02json/app/Http/Controllers/UsuarioController.php
@@ -19,8 +19,11 @@ class UsuarioController extends Controller{
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255|min:5',
+        ]);
+        
         $nuevoUsuario = [
             'id' => count($this->usuarios) + 1,
             'name' => $request->input('name')
