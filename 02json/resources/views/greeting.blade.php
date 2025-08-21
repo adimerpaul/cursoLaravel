525a449a5c66438db4ad339e055f46f365973774
adimerpaul <adimer101@gmail.com>
2025-08-20 20:01:30 -0400

    Funcioano plantaillas blade con usuario y condicioanes

--- 02json/resources/views/greeting.blade.php
+++ 02json/resources/views/greeting.blade.php
@@ -8,5 +8,16 @@
 </head>
 <body>
     <h1>Hello, {{ $name }}</h1>
+    <ul>
+        @foreach ($usuarios as $usuario)
+            <li>{{ $usuario['name'] }} - {{ implode(', ', $usuario['profeciones'] ?? []) }}</li>
+        @endforeach
+    </ul>
+
+    @if ($num1 > $num2)
+        <p>Num1 {{ $num1 }} es mayor</p>
+    @else
+        <p>Num2 {{ $num2 }} es mayor</p>
+    @endif
 </body>
 </html>
--- 02json/routes/web.php
+++ 02json/routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('greeting', ['name' => 'Adimer']);
+    $usuarios = [
+        [
+            'name' => 'Adimer',
+            'profeciones' => ['Desarrollador', 'Diseñador']
+        ],
+        [
+            'name' => 'Finn'
+        ],
+        [
+            'name' => 'Maria santis',
+            'profeciones' => ['Analista']
+        ]
+    ];
+    $num1 = 89;
+    $num2 = 20;
+    // return response()->json($array);
+
+    return view('greeting', ['name' => 'Adimer', 'usuarios' => $usuarios, 'num1' => $num1, 'num2' => $num2]);
 });
