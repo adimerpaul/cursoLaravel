83d1ce7edded653d67bb977acf606d64ee41fdee
adimerpaul <adimer101@gmail.com>
2025-08-21 20:38:05 -0400

    La

--- 04basededatos/app/Http/Controllers/UserController.php
+++ 04basededatos/app/Http/Controllers/UserController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    function index(){
+        return User::all();
+    }
+    function store(){
+        $user = new User();
+        $user->name = request('name');
+        $user->email = request('email');
+        $user->password = bcrypt(request('password'));
+        $user->save();
+        return $user;
+    }
+    function update($id, Request $request){
+        $user = User::find($id);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        if ($request->input('password')) {
+            $user->password = bcrypt($request->input('password'));
+        }
+        $user->save();
+        return $user;
+    }
+    function destroy($id){
+        $user = User::find($id);
+        if ($user) {
+            $user->delete();
+            return response()->json(['message' => 'User deleted successfully'], 200);
+        }
+        return response()->json(['message' => 'User not found'], 404);
+    }
 }
