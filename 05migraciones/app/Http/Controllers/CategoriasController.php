aa5a03a5655bff6c0202fa4ebafe78aeae93d60d
adimerpaul <adimer101@gmail.com>
2025-08-25 20:48:17 -0400

    Creado controller y apis de categorias

---
+++ 05migraciones/app/Http/Controllers/CategoriasController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CategoriasController extends Controller{
+    function index(){
+        // $categorias = DB::select('select id,nombre from categorias');
+        // $categorias = DB::table('categorias')->select('id','nombre','descripcion')->get();
+        // $categorias = DB::table('categorias')->orderBy('id','desc')->get();
+        // $categorias = DB::table('categorias')->where('id', 1)->orWhere('id', 2)->get();
+
+        $productos = DB::table('productos as p')
+            ->selectRaw('p.categoria_id, count(*) as total')
+            ->groupBy('p.categoria_id')
+            ->get();
+
+        $categoriasCount = DB::table('categorias')->count();
+
+        $productoPagination = DB::table('productos')->paginate(2);
+        return $productoPagination;
+    }
+}
