bb1b49e10eea6385df0b3c45886bca88c520a7af
adimerpaul <adimer101@gmail.com>
2025-08-28 20:15:56 -0400

    seeder de 3000 usuarios

--- 08archivos/routes/api.php
+++ 08archivos/routes/api.php
@@ -3,9 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UploadController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/user', [UserController::class, 'index']);
 
 Route::post('/upload', [UploadController::class, 'upload']);
