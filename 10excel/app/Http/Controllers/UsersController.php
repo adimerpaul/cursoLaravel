7efa6f5bf64b299ed1581465aef5e59c53b0f222
adimerpaul <adimer101@gmail.com>
2025-09-02 20:18:29 -0400

    creado proyecto 10 excel

---
+++ 10excel/app/Http/Controllers/UsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Exports\UsersExport;
+use App\Imports\UsersImport;
+use Maatwebsite\Excel\Facades\Excel;
+
+
+class UsersController extends Controller
+{
+    public function export() 
+    {
+        return Excel::download(new UsersExport, 'users.xlsx');
+    }
+
+    public function import() 
+    {
+        Excel::import(new UsersImport, 'users.xlsx');
+
+        return 'Import completed';
+    }
+}
--- 10excel/routes/web.php
+++ 10excel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('export', [UsersController::class, 'export']);
+Route::get('import', [UsersController::class, 'import']);
+
