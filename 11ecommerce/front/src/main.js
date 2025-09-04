5d17441c51c7ab6db3454d092f7b46872a150ad6
adimerpaul <adimer101@gmail.com>
2025-09-03 20:34:48 -0400

    funcioano paginacion

--- 11ecommerce/front/src/main.js
+++ 11ecommerce/front/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
+import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import HomeView from './pages/HomeView.vue'
+import AboutView from './pages/AboutView.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/about', component: AboutView },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
