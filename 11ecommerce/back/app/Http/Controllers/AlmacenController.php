a9c5376c0015fcbbd4cbd2017bcebf229dd117f3
adimerpaul <adimer101@gmail.com>
2025-09-11 21:01:47 -0400

    Colcoado validacione

--- 11ecommerce/back/app/Http/Controllers/AlmacenController.php
+++ 11ecommerce/back/app/Http/Controllers/AlmacenController.php
@@ -11,6 +11,12 @@ class AlmacenController extends Controller
         return Almacen::all();
     }
     public function store(Request $request){
+        $validated = $request->validate([
+            'nombre' => 'required|string|max:255',
+            'direccion' => 'nullable|string|max:255',
+            'telefono' => 'nullable|string|max:20',
+            'sucursal_id' => 'required|exists:sucursales,id',
+        ]);
         Almacen::create($request->all());
         return response()->json(['message' => 'Almacen creado exitosamente'], 201);
     }
